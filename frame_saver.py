0aa2c113ec3f4138fb604485ff8a7d274630e499
Anton Voronov <voronov.a.dm@yandex.ru>
2021-08-23 23:14:02 +0300

    добавил рабочий воркер потока видео

--- frame_saver.py
+++ frame_saver.py
@@ -11,7 +11,7 @@ import cv2
 import os
 
 DEFAULT_CHUNK_SIZE = 17000
-TIME_FORMAT = "%m/%d/%Y_%H:%M:%S"
+TIME_FORMAT = "%m-%d-%Y_%H:%M:%S"
 DEFAULT_FORMAT = ".jpg"
 
 def parse_url(to_parse: str):
@@ -31,6 +31,7 @@ def get_logger(level=logging.INFO) -> logging.Logger:
     logger.setLevel(level)
     return logger
 
+
 class CamReader(threading.Thread):
     """
         Класс для непрерывного чтения в потоке
@@ -38,7 +39,7 @@ class CamReader(threading.Thread):
     _stream: cv2.VideoCapture
     _logger: logging.Logger
     _frame: Optional = None
-    _stamp: float = 0
+    _stamp: datetime.datetime = 0
     _address: str
     _reconnect_counter: int = 0
     _fps: int
@@ -64,7 +65,7 @@ class CamReader(threading.Thread):
         self._logger.info(
             f"[{self.name}]::_connect:Connect success to the camera stream {self._address}")
 
-    def reconnect(self):
+    def _reconnect(self):
         self._stream = cv2.VideoCapture(self._address)
         self._stream.set(cv2.CAP_PROP_FPS, self._fps)
         self._stream.set(cv2.CAP_PROP_BUFFERSIZE, 2)
@@ -74,20 +75,36 @@ class CamReader(threading.Thread):
     def _prepare_folder(self, path: str):
         ip, user, password = parse_url(self._address)
         self._ip = ip
+        target_dir = os.path.join(path, ip)
         try:
-            if not os.path.exists(os.path.join(path, ip)):
-                os.mkdir(os.path.join(path, ip))
-            self._save_folder = os.path.join(path, ip)
+            if not os.path.exists(target_dir):
+                os.mkdir(target_dir)
+            self._save_folder = target_dir
         except FileExistsError:
             self._save_folder = os.path.join(os.getcwd(), ip)
             os.mkdir(self._save_folder)
         self._logger.info(
             f"[{self.name}]::_prepare_folder: Save folder for cam [{ip}] > [{self._save_folder}]")
 
-    def _save_frame(self, frame: np.ndarray, timestamp: datetime.datetime):
-        file_name = f"frame_[{timestamp.strftime(TIME_FORMAT)}]_IP[{self._ip}.jpg]"
+    def save_frame(self):
+        self._logger.debug(
+            f"[{self.name}]::save_frame: Saving frame from stream [{self._address}]")
+        if not isinstance(self._frame, np.ndarray):
+            self._logger.debug(
+                f"[{self.name}]::save_frame: Frame is empty!")
+            return
+        file_name = f"frame_{self._stamp.strftime(TIME_FORMAT)}.jpg"
         filepath = os.path.join(self._save_folder, file_name)
-        cv2.imwrite(filepath, frame)
+        cv2.imwrite(os.path.abspath(filepath), self._frame)
+        self._logger.debug(f"[{self.name}]::save_frame: Frame saved by path [{filepath}]]")
+
+    def show_frame(self):
+        if not isinstance(self._frame, np.ndarray):
+            self._logger.debug(
+                f"[{self.name}]::show_frame: Frame is empty!")
+            return
+        cv2.imshow(f"{self.name}", self._frame)
+        cv2.waitKey(1)
 
     def run(self) -> None:
         self._logger.info(f"[{self.name}]::_connect: Starting streaming")
@@ -96,18 +113,14 @@ class CamReader(threading.Thread):
             _, frame = self._stream.read()
             if isinstance(frame, np.ndarray):
                 self._frame = frame
-                self._stamp = time.time()
-            elif frame is None and self._reconnect_counter > 100:
-                self._connect()
-                self._reconnect_counter = 0
+                self._stamp = datetime.datetime.now()
             elif frame is None:
                 self._logger.debug(f"[{self.name}]::run:Camera stream is empty")
-                self._reconnect_counter += 1
+                self._reconnect()
             else:
                 self._logger.warning(f"[{self.name}]::run: Unsupported situation!")
 
     def get_frame(self):
-        self._reconnect_counter += 1
         return self._frame, self._stamp
 
 
@@ -128,14 +141,30 @@ if __name__ == '__main__':
         "rtsp://root:Video5000!@172.30.71.110/axis-media/media.amp?"
     ]
 
-    SAVE_FOLDER = "D:\\smoke_dataset"
-    recs = [HyperCamReader(
+    SAVE_FOLDER = "./flow_data"
+    FPS = 0.2
+    log.info(f"Creating Threads")
+    recs = [CamReader(
         name=f"Cam_writer_{(itt+100)-1}",
         logger=log,
         address=cam,
         save_folder=SAVE_FOLDER
     ) for itt, cam in enumerate(cameras)]
+    log.info(f"Creating have been created!")
+    log.info(f"Starting Threads")
     for rec in recs:
         rec.start()
-    for rec in recs:
-        re
+    log.info(f"Threads starter")
+    start_time = time.time()
+    log.info(f"Starting checkout loop")
+
+    while True:
+        # log.info(f"Parsing Cams")
+        for cam in recs:
+            cam.save_frame()
+            cam.show_frame()
+        # log.info(f"Cams parsing finished...")
+        diff = (start_time + (1 / FPS))
+        if diff > time.time():
+            time.sleep(diff - time.time())
+        start_time = time.time()
